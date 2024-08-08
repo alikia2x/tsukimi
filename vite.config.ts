c39486b205dfbe76e45503d7ffc6a1bc78e39c46
alikia2x <alikia2x@outlook.com>
2024-08-08 18:37:38 +0800

    fix: resources in build files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { createHtmlPlugin } from "vite-plugin-html";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,9 @@ export default defineConfig({
 					injectScript: `<script type="module" src="/src/main.tsx"></script>`
 				}
 			}
+		}),
+		viteStaticCopy({
+			targets: [{ src: "resources/manual-article/**/*", dest: "resources/manual-article" }]
 		})
 	]
 });
