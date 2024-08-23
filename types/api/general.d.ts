892ea971f2ee29a7b6fadf279f90f33e710125bd
alikia2x <alikia2x@outlook.com>
2024-08-24 03:15:31 +0800

    update: type defention for post

--- types/api/general.d.ts
+++ types/api/general.d.ts
@@ -6,4 +6,46 @@ to easily support a new social media platform by
 writing adapters that conform to these interfaces.
 */
 
-interface PostItem {}
+interface Post {
+	id: string;
+	uri: string;
+	time: string;
+	author: Account;
+	content: string;
+    contentType: "HTML" | "markdown" | "plaintext";
+	// Whatever the content type is, this represents the plain text form of the post
+	text: string | null;
+	visibility: "public" | "unlisted" | "followers" | "direct";
+	sensitive: boolean;
+	warningText: string | null; // Used for CW(NSFW)
+	files: FileAttachment[];
+	application?: {
+		name: string;
+		website: string | null;
+	};
+	mentions: Account[];
+	tags: HashTag[];
+	emojis: CustomEmoji[];
+	reactions?: Reaction[];
+	repostCount: number;
+	reactionCount?: number;
+    likeCount: number;
+	replyCount: number;
+	url: string | null;
+	replyTo?: string;
+    repostFrom?: string;
+	poll?: Poll;
+	linkCard?: LinkCard;
+	language: string | null; // ISO 639 Part 1 two-letter language code
+	editTime?: string; // ISO 8601 Datetime
+	favourited?: boolean;
+	reposted?: boolean;
+	muted?: boolean;
+	bookmarked?: boolean;
+	pinned?: boolean;
+	filtered?: FilterResult[];
+}
+
+interface FileAttachment {
+	
+}
